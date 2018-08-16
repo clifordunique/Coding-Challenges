3fbd708a65566d8078cb7bb7031d4c51e3581483
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-08-16 18:51:42 -0400

    Add files via upload

---
+++ 78 Simple Particles/particle.js
@@ -0,0 +1,24 @@
+class Particle {
+
+	constructor(x, y, d, xspeed, yspeed) {
+		this.x = x;
+		this.y = y;
+		this.d = d;
+		this.xspeed = xspeed;
+		this.yspeed = yspeed;
+		this.alpha = 255
+	}
+
+	update() {
+		this.x += this.xspeed;
+		this.y -= this.yspeed;
+		this.alpha -= 10;
+	}
+
+	show() {
+		noStroke();
+		fill(255, this.alpha);
+		ellipse(this.x, this.y, this.d)
+	}
+
+}
