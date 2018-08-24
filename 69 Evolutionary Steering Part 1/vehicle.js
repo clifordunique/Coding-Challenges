2c1c854e9dda1ad50a641a1f7f0de40aef9252d3
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-08-24 16:30:39 -0400

    Update vehicle.js

--- 69 Evolutionary Steering Part 1/vehicle.js
+++ 69 Evolutionary Steering Part 1/vehicle.js
@@ -1,10 +1,10 @@
 function Vehicle(x, y) {
-
+	
 	this.pos = createVector(x, y);
 	this.vel = createVector(0, -2);
-	this.acc = createVector(0, 0);
+	this.acc = createVector();
 	this.r = 6;
-	this.maxSpeed = 5;//abaility to seek the target
+	this.maxSpeed = 5;
 	this.maxForce = 0.5;
 
 	this.update = function() {
@@ -21,43 +21,39 @@ function Vehicle(x, y) {
 	this.eat = function(list) {
 		var record = Infinity;
 		var closest = -1;
+
 		for(let i = 0; i < list.length; i++) {
-			var d = this.pos.dist(list[i]);
+			var d = dist(this.pos.x, this.pos.y, list[i].x, list[i].y);
 			if(d < record) {
 				record = d;
 				closest = i;
 			}
 		}
 
-		if(record < 5) {//if reach it eat it and remove it
-			list.splice(closest , 1);
+		if(record < 5) {
+			list.splice(closest, 1);
+		} else if(closest !== -1){
+			this.seek(list[closest]);
 		}
 
-		this.seek(list[closest]);
 	}
 
-
+	//basic principle how to steer 
 	this.seek = function(target) {
-		//this is the desired velocity
-		var desired = p5.Vector.sub(target, this.pos);
+		var desiredVel = p5.Vector.sub(target, this.pos);
+		desiredVel.setMag(this.maxSpeed);
 
-		//Scale the maximum speed
-		desired.setMag(this.maxSpeed);//此向量的大小设置为this.maxSpeed
-
-		//Steering = desired minus velocity
-		var steer = p5.Vector.sub(desired, this.vel);
-		steer.limit(this.maxForce);
+		var steer = p5.Vector.sub(desiredVel, this.vel);
+		steer.mult(this.maxForce);
 
 		this.applyForce(steer);
 	}
 
 	this.display = function() {
-		
-    	//Calculate the angle of rotation for this vector
 		var theta = this.vel.heading() + PI / 2;
+		stroke(255);
 		fill(127);
-		stroke(200);
-		strokeWeight(1);
+		strokeWeight(2);
 
 		push();
 			translate(this.pos.x, this.pos.y);
@@ -66,20 +62,8 @@ function Vehicle(x, y) {
 				vertex(0, -this.r * 2);
 				vertex(-this.r, this.r * 2);
 				vertex(this.r, this.r * 2);
-			endShape();
+			endShape(CLOSE);
 		pop();
-
-    }
+	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
