3e0388fecb8205bb31d9d9ab3309d4f9d4a2b715
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-08-24 16:30:17 -0400

    Update sketch.js

--- 69 Evolutionary Steering Part 1/sketch.js
+++ 69 Evolutionary Steering Part 1/sketch.js
@@ -12,56 +12,29 @@ function setup() {
 	for(let i = 0; i < 10; i++) {
 		poison.push(createVector(random(width), random(height)));
 	}
-}
+} 
 
 function draw() {
 	background(0);
 
-	// var target = createVector(mouseX, mouseY);
-	// fill(127);
-	// stroke(200);
-	// strokeWeight(2);
-	// ellipse(target.x, target.y, 48, 48);
+	var target = createVector(mouseX, mouseY);
+	fill(127);
+	stroke(255);
+	ellipse(target.x, target.y, 20, 20);
 
 	for(let i = 0; i < food.length; i++) {
 		fill(0, 255, 0);
 		noStroke();
-		ellipse(food[i].x, food[i].y, 8, 8);
+		ellipse(food[i].x, food[i].y, 6, 6);
 	}
 	for(let i = 0; i < poison.length; i++) {
 		fill(255, 0, 0);
 		noStroke();
-		ellipse(poison[i].x, poison[i].y, 8, 8);
+		ellipse(poison[i].x, poison[i].y, 6, 6);
 	}
 
 	vehicle.eat(food);
-	//vehicle.seek(food);
+	//vehicle.seek(target);
 	vehicle.update();
 	vehicle.display();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
