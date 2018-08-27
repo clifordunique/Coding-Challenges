4e9111b50acfcf15ad089239d8faef935bc9ae52
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-08-27 14:29:13 -0400

    Update sketch.js

--- 69 Evolutionary Steering Part 1/sketch.js
+++ 69 Evolutionary Steering Part 1/sketch.js
@@ -1,40 +1,53 @@
-let vehicle;
+let vehicles = [];
+let target;
+
 let food = [];
 let poison = [];
 
 function setup() {
 	createCanvas(600, 400);
-	vehicle = new Vehicle(width / 2, height / 2);
+
+	for(let i = 0; i < 4; i++) {
+		vehicles.push(new Vehcile(random(width), random(height)));
+	}
 
 	for(let i = 0; i < 10; i++) {
 		food.push(createVector(random(width), random(height)));
 	}
+
 	for(let i = 0; i < 10; i++) {
 		poison.push(createVector(random(width), random(height)));
 	}
-} 
+	
+}
 
 function draw() {
 	background(0);
 
-	var target = createVector(mouseX, mouseY);
-	fill(127);
+	//-=-=-=-=-=-=-=-=-mouse cursor-=-=-=-=-=-=-=-=-
+	target = createVector(mouseX, mouseY);
 	stroke(255);
-	ellipse(target.x, target.y, 20, 20);
+	fill(127);
+	ellipse(target.x, target.y, 25, 25);
+	//-=-=-=-=-=-=-=-=-mouse cursor-=-=-=-=-=-=-=-=-
 
 	for(let i = 0; i < food.length; i++) {
 		fill(0, 255, 0);
 		noStroke();
-		ellipse(food[i].x, food[i].y, 6, 6);
-	}
-	for(let i = 0; i < poison.length; i++) {
-		fill(255, 0, 0);
-		noStroke();
-		ellipse(poison[i].x, poison[i].y, 6, 6);
+		ellipse(food[i].x, food[i].y, 5, 5);
 	}
 
-	vehicle.eat(food);
-	//vehicle.seek(target);
-	vehicle.update();
-	vehicle.display();
+	for(let i = 0; i < poison.length; i++) {
+		fill(0238, 0, 255);
+		noStroke();
+		ellipse(poison[i].x, poison[i].y, 5, 5);
+	}
+
+	for(let vehcile of vehicles) {
+		vehcile.eat(food);
+	//vehcile.seek(target);
+		vehcile.update();
+		vehcile.show();
+	}
 }
+
