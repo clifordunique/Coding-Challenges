23e39d48bdff48d218c979680bb2d0570dc4e545
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-08-27 14:29:32 -0400

    Update vehicle.js

--- 69 Evolutionary Steering Part 1/vehicle.js
+++ 69 Evolutionary Steering Part 1/vehicle.js
@@ -1,11 +1,16 @@
-function Vehicle(x, y) {
-	
+function Vehcile(x, y) {
+
 	this.pos = createVector(x, y);
-	this.vel = createVector(0, -2);
+	this.vel = createVector();
 	this.acc = createVector();
 	this.r = 6;
 	this.maxSpeed = 5;
 	this.maxForce = 0.5;
+
+	this.applyForce = function(force) {
+		this.acc.add(force);
+	}
+
 
 	this.update = function() {
 		this.vel.add(this.acc);
@@ -14,56 +19,56 @@ function Vehicle(x, y) {
 		this.acc.mult(0);
 	}
 
-	this.applyForce = function(force) {
-		this.acc.add(force);
-	}
-
 	this.eat = function(list) {
 		var record = Infinity;
 		var closest = -1;
 
 		for(let i = 0; i < list.length; i++) {
-			var d = dist(this.pos.x, this.pos.y, list[i].x, list[i].y);
+			var d = dist(this.pos.x, this.pos.y, list[i].x, list[i].y);//easy understand
+			//var d = this.pos.dist(list[i]);//tutorial code
 			if(d < record) {
 				record = d;
 				closest = i;
 			}
+		
+			if(d < 5) {
+				list.splice(i, 1); 
+			} else if (record !== -1) {
+				this.seek(list[closest]);
+			}
+		
 		}
-
-		if(record < 5) {
-			list.splice(closest, 1);
-		} else if(closest !== -1){
-			this.seek(list[closest]);
-		}
-
+		
 	}
 
-	//basic principle how to steer 
 	this.seek = function(target) {
-		var desiredVel = p5.Vector.sub(target, this.pos);
-		desiredVel.setMag(this.maxSpeed);
-
-		var steer = p5.Vector.sub(desiredVel, this.vel);
-		steer.mult(this.maxForce);
+		var desired = p5.Vector.sub(target, this.pos);
+		desired.setMag(this.maxSpeed);
+		
+		var steer = p5.Vector.sub(desired, this.vel);
+		steer.limit(this.maxForce);
 
 		this.applyForce(steer);
 	}
 
-	this.display = function() {
+	this.show = function() {
 		var theta = this.vel.heading() + PI / 2;
+
+		// translate(this.pos.x, this.pos.y);
+		// rotate(theta);
 		stroke(255);
 		fill(127);
-		strokeWeight(2);
-
+		
 		push();
+		beginShape();
 			translate(this.pos.x, this.pos.y);
 			rotate(theta);
-			beginShape();
-				vertex(0, -this.r * 2);
-				vertex(-this.r, this.r * 2);
-				vertex(this.r, this.r * 2);
-			endShape(CLOSE);
+			vertex(0, -this.r * 2);
+			vertex(-this.r, this.r * 2);
+			vertex(this.r, this.r * 2);
+		endShape(CLOSE);
 		pop();
+		
 	}
 
 }
