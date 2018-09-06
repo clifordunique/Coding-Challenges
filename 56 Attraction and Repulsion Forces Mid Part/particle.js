6aa23223f9a698799417323af9a91adc006e0dea
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-09-06 15:49:51 -0400

    Update particle.js

--- 56 Attraction and Repulsion Forces Mid Part/particle.js
+++ 56 Attraction and Repulsion Forces Mid Part/particle.js
@@ -8,35 +8,42 @@ function Particle(x, y) {
 	//this.vel = createVector();
 	this.acc = createVector();
 
-	//F = G / d2
+	//F = (G * M1 * M2) / d^2
 	this.attracted = function(target, j) {
-		var force = p5.Vector.sub(target, this.pos);
-		var dsquared = force.magSq();
-		dsquared = constrain(dsquared, 5, 100)
+		//var d = dist(this.pos.x, this.pos.y, target.x, target.y);//error
+		//dir = target - current position
+		var force = p5.Vector.sub(target, this.pos);//!!!!!!!!!!!
+		var dSquare = force.magSq();
+		dSquare = constrain(dSquare, 5, 100);
 		var G = 50;
-		var strength = G / dsquared;
-		force.setMag(strength);
+		var strength = G / dSquare;
+		force.setMag(strength);//!!!!!!!!!!!!!!!!
 
 		if(j % 2 == 1) {
-			force.mult(-1);//replusion
+			force.mult(-1);
 		}
-		
-		
+
+		this.applyForce(force);
+	}
+
+	this.applyForce = function(force) {
 		this.acc.add(force);
 	}
 
 	this.update = function() {
 		this.vel.add(this.acc);
-		this.vel.limit(5);
+		this.vel.limit(5);//!!!!!!!!!!!
 		this.pos.add(this.vel);
 		this.acc.mult(0);
 	}
 
 	this.show = function() {
-		stroke(255, 250);
-		strokeWeight(1);
+		noFill();
+		stroke(255, 255, 255, 250);
+		strokeWeight(2);
+		//point(this.pos.x, this.pos.y);
 		line(this.pos.x, this.pos.y, this.prev.x, this.prev.y);
-
+		
 		this.prev.x = this.pos.x;
 		this.prev.y = this.pos.y;
 	}
