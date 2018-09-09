b2eb710d1419aade6c4a19b3519888de905441d4
Bruce Ted <35704708+zheyuanzhou@users.noreply.github.com>
2018-09-09 13:31:55 -0400

    Update sketch.js

--- 35 Traveling Saleperson Part 1/sketch.js
+++ 35 Traveling Saleperson Part 1/sketch.js
@@ -1,15 +1,17 @@
 var cities = [];
-var totalCities = 10;
+var totalCities = 15;
 
 var recordDistance;
 var bestEver;
 
 function setup() {
-	createCanvas(600, 400);
+	createCanvas(500, 500);
+
 	for(let i = 0; i < totalCities; i++) {
 		cities.push(createVector(random(width), random(height)));
 	}
-	recordDistance = calaDistance(cities);
+
+	recordDistance = calDistance(cities);
 	bestEver = cities.slice();
 }
 
@@ -17,65 +19,63 @@ function draw() {
 	background(0);
 
 	for(let i = 0; i < cities.length; i++) {
-		noStroke();
-		fill(255);
-		ellipse(cities[i].x, cities[i].y, 5, 5);
-	}
-
-	//-=-=-=-=-=-=-=-=-=-seraching line-=-=-=-=-=-=-=-=-=-
-	beginShape();
-	for(let i = 0; i < cities.length; i++) {
 		stroke(255);
-		noFill();
-		vertex(cities[i].x, cities[i].y);
+		strokeWeight(5);
+		point(cities[i].x, cities[i].y);
 	}
-	endShape();
 
-	//-=-=-=-=-=-=-=-=-=-best line-=-=-=-=-=-=-=-=-=-
+	//-=-=-=-=-Searching line-=-=-=-=-
+	//This is only connect with random points with each other
 	beginShape();
-	for(let i = 0; i < cities.length; i++) {
-		stroke(255, 0, 200);
-		noFill();
-		vertex(bestEver[i].x, bestEver[i].y);
-	}
+		for(let i = 0; i < cities.length - 1; i++) {
+			stroke(255);
+			strokeWeight(2);
+			line(cities[i].x, cities[i].y, cities[i + 1].x, cities[i + 1].y);
+		}
 	endShape();
-	
-	//-=-=-=-=-=-=-=-=-=-searching line-=-=-=-=-=-=-=-=-=-
-// 	for(let i = 0; i < cities.length - 1; i++) {
-// 		stroke(255);
-// 		line(cities[i].x, cities[i].y, cities[i + 1].x, cities[i + 1].y);
-// 	}
 
- 	//-=-=-=-=-=-=-=-=-=-best line-=-=-=-=-=-=-=-=-=-
-// 	for(let i = 0; i < bestEver.length - 1; i++) {
-// 		stroke(200, 0, 100);
-// 		line(bestEver[i].x, bestEver[i].y, bestEver[i + 1].x, bestEver[i + 1].y);
-// 	}
+	beginShape();
+		for(let i = 0; i < cities.length; i++) {
+			stroke(255, 0, 200);
+			strokeWeight(2);
+			noFill();
+			//line(cities[i].x, cities[i].y, cities[i + 1].x, cities[i + 1].y);
+			vertex(bestEver[i].x, bestEver[i].y);
+		}
+	endShape();
 
+	//swap array can make your result more random amd more detail to all points
 	var i = floor(random(cities.length));
 	var j = floor(random(cities.length));
 	swap(cities, i, j);
 
-	var d = calaDistance(cities);
+	//!!!!!!!!!!!!!!!!!
+	var d = calDistance(cities);
 	if(d < recordDistance) {
 		recordDistance = d;
+		//The slice() method returns a shallow copy of a portion of an array 
+		//into a new array object selected from begin to end
 		console.log(recordDistance);
 		bestEver = cities.slice();
 	}
+	//!!!!!!!!!!!!!!!!!
 }
 
-function swap(array, i, j) {
-	var temp = array[i];
-	array[i] = array[j];
-	array[j] = temp;
+function swap(arr, i, j) {
+	var temp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = temp;
 }
 
-function calaDistance(points) {
+
+function calDistance(points) {
 	var sum = 0;
 	for(let i = 0; i < points.length - 1; i++) {
-		var d = dist(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);//pat attention to the parameter
+		var d = dist(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
 		sum += d;
 	}
 	return sum;
 }
+
+
 
